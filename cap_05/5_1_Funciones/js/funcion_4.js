dc5f7a5240629c72ecc21d70f690276d8beb7557
Julio Mata <julioematasv@gmail.com>
2022-12-16 09:54:23 +0100

    Creado archivo funcion_34html, pasando  varios arametros a la función

---
+++ cap_05/5_1_Funciones/js/funcion_4.js
@@ -0,0 +1,9 @@
+
+document.write("<h1>Texto Para Pruebas de Color</h1>");
+
+
+
+function cambiarColor(colorDeFondo, colorDeTexto){
+    document.bgColor = colorDeFondo;
+    document.fgColor = colorDeTexto;
+}
