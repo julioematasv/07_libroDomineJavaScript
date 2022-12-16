aa46d5c0a8121247313ec4cd156fffd8f47453ec
Julio Mata <julioematasv@gmail.com>
2022-12-16 10:18:01 +0100

    Creado archivo fpaso_1.html y paso_2.html, viendo el ambito de las variables, local y global

---
+++ cap_05/5_3_Variables_Publicas_y_Privadas/js/paso_1.js
@@ -0,0 +1,12 @@
+// inicializando la variable
+let valor = 10;
+function doblar(dato) {
+    dato *= 2;
+    document.write("El doble del valor es " + dato + "<br>");
+}
+
+document.write("El valor es " + valor + "<br>");
+doblar(valor);  //llamamos a la funcion pasándole la variable.
+
+document.write("El valor sigue siendo " + valor);
+//La variable original no se ha modificado.
