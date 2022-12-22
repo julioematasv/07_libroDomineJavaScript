0dfb50be393b7c482316cdfd39e8dcfb946352da
Julio Mata <julioematasv@gmail.com>
2022-12-22 10:03:21 +0100

    Arrays, creados archivos crear_matriz_1. html y js

---
+++ cap_05/5_3_Matrices/js/crear_matriz_1.js
@@ -0,0 +1,23 @@
+var nombres = new Array("Nacho", "Areck", "Laura", "Sonia", "Gonzalo", "Eva", "Pedro");
+
+var apellidos = new Array(6);
+
+// A continuación declaramos el contenido del array apellidos:
+apellidos[0] = "Perez";
+apellidos[1] = "López";
+apellidos[2] = "Mata";
+apellidos[3] = "Chávez";
+apellidos[4] = "García";
+apellidos[5] = "Martinez";
+
+
+
+document.write(nombres[0] + " " + apellidos[0] +  "<br>");
+document.write(nombres[1] + " " + apellidos[1] +  "<br>");
+document.write(nombres[2] + " " + apellidos[2] +  "<br>");
+document.write(nombres[3] + " " + apellidos[3] +  "<br>");
+document.write(nombres[4] + " " + apellidos[4] +  "<br>");
+document.write(nombres[5] + " " + apellidos[5] +  "<br>");
+
+apellidos[6] = "Acosta";
+document.write(nombres[6] + " " + apellidos[6] +  "<br>");
