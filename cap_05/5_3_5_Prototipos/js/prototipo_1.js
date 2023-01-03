757a9961f7e4b08f08debe54cd380c7a58d55447
Julio Mata <julioematasv@gmail.com>
2023-01-03 12:42:06 +0100

    Apartado de Prototipos, creando prototipos archivo: prototipo_1 .html y .js

---
+++ cap_05/5_3_5_Prototipos/js/prototipo_1.js
@@ -0,0 +1,8 @@
+
+Array.prototype.tipo = "";
+
+var nombres = new Array("José", "Laura", "Antonio", "Susana", "Andrea");
+
+nombres.tipo = "alfanumérico";
+
+document.write("La matriz 'nombres' contiene datos del tipo: " + nombres.tipo);
